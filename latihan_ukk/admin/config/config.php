08456db575043a48398843ab49be1134ed36aedc
rahmisriwahyuni0 <93960823+rahmisriwahyuni0@users.noreply.github.com>
2022-03-15 15:33:16 +0700

    Add files via upload

---
+++ latihan_ukk/admin/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://locallhost/latihan_ukk/public');
+
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','portofolio');
---
+++ latihan_ukk/admin/controllers/portofolio.php
@@ -0,0 +1,9 @@
+<?php
+
+class portofolio extends Controller
+{
+    public function index()
+    {
+        $this->view('portofolio/index');
+    }
+}
